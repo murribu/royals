c3482307d179389c769bff7f06701cda173b4133
Cory Martin <murribu@gmail.com>
2016-09-30 23:34:27 -0500

    add 'unknown' stats pitch type

--- database/seeds/PitchTypesSeeder.php
+++ database/seeds/PitchTypesSeeder.php
@@ -187,6 +187,7 @@ class PitchTypesSeeder extends Seeder{
                     ),
                     'stats' => array(
                         'NA',
+                        'U',
                         ''
                     ),
                 ),
